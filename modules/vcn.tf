d5189b88675b27e189759a9003ee527bac190c6b
salmaaazara <salma.a.azara@gmail.com>
2026-08-25 00:14:11 +0400

    Add files via upload

---
+++ modules/vcn.tf
@@ -0,0 +1,6 @@
+resource "oci_core_vcn" "main" {
+  compartment_id = var.compartment_ocid
+  cidr_blocks    = [var.vcn_cidr]
+  display_name   = "${var.project_name}-vcn"
+  dns_label      = "cloud-grad-project"
+} 
