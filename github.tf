38542fbd25a06ecb84904e2519a18f5cab3b1126
Beshoy-Sorial <beshoy.sorial.goda@gmail.com>
2026-08-26 19:24:42 +0300

    fix: remove organization prefix from github repo name

--- github.tf
+++ github.tf
@@ -1,6 +1,6 @@
 # This automatically adds the new CLUSTER_OCID to your GitHub Repository Secrets
 resource "github_actions_secret" "cluster_ocid" {
-  repository      = var.github_repo
+  repository      = replace(var.github_repo, "${var.github_owner}/", "")
   secret_name     = "CLUSTER_OCID"
   value           = module.oke.cluster_id
 }
