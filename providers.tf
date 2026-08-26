0c6335e7f6bb14b89242a6c6954764486d81958d
Beshoy-Sorial <beshoy.sorial.goda@gmail.com>
2026-08-26 23:29:29 +0300

    feat: configure OCI S3-compatible backend for Terraform state

--- providers.tf
+++ providers.tf
@@ -1,4 +1,12 @@
 terraform {
+  backend "s3" {
+    bucket                      = "terraform-states"
+    key                         = "oke-cluster/terraform.tfstate"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    force_path_style            = true
+  }
   required_providers {
     oci = {
       source  = "oracle/oci"
